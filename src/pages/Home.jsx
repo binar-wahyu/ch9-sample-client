477478719080f71a104a34b70130a5ecbe74a1e9
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-07-16 15:19:11 +0800

    Add article list fetch data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,46 @@
+import { useState, useEffect } from "react";
 import { Card, Row, Col, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function Home() {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    let ignore = false;
+
+    async function getData() {
+      setLoading(true);
+      const response = await fetch("http://localhost:4000/articles");
+      const data = await response.json();
+
+      if (ignore) return;
+
+      setLoading(false);
+      setArticles(data);
+    }
+
+    getData();
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
   return (
     <Container className="mt-3">
       <Row className="g-3">
-        {[...Array(5)].map((_, index) => (
-          <Col lg={4} key={index}>
-            <Card body>
-              <Link to={`/article/${index + 1}`}>Article {index + 1}</Link>
-            </Card>
-          </Col>
-        ))}
+        {loading ? (
+          <h3>Loading...</h3>
+        ) : (
+          articles.map((article) => (
+            <Col lg={4} key={article.id}>
+              <Card body>
+                <Link to={`/article/${article.id}`}>{article.title}</Link>
+              </Card>
+            </Col>
+          ))
+        )}
       </Row>
     </Container>
   );
