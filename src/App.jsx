331339093371426473372e1dc861f6acc9c94999
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-07-19 16:59:24 +0800

    Complete auth firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ProvideAuth } from "./context/auth";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import ContactUs from "./pages/ContactUs";
 import BranchBali from "./pages/BranchBali";
 import BranchJakarta from "./pages/BranchJakarta";
@@ -10,21 +13,25 @@ import ArticleDetail from "./pages/ArticleDetail";
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Header />
+      <ProvideAuth>
+        <div className="App">
+          <Header />
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tentang-kami" element={<h1>Tentang kami</h1>} />
-          <Route path="/hubungi-kami" element={<ContactUs />}>
-            <Route index element={<h1>Silahkan pilih cabang</h1>} />
-            <Route path="bali" element={<BranchBali />} />
-            <Route path="jakarta" element={<BranchJakarta />} />
-            <Route path="padang" element={<BranchPadang />} />
-          </Route>
-          <Route path="/article/:id" element={<ArticleDetail />} />
-        </Routes>
-      </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/tentang-kami" element={<h1>Tentang kami</h1>} />
+            <Route path="/hubungi-kami" element={<ContactUs />}>
+              <Route index element={<h1>Silahkan pilih cabang</h1>} />
+              <Route path="bali" element={<BranchBali />} />
+              <Route path="jakarta" element={<BranchJakarta />} />
+              <Route path="padang" element={<BranchPadang />} />
+            </Route>
+            <Route path="/article/:id" element={<ArticleDetail />} />
+          </Routes>
+        </div>
+      </ProvideAuth>
     </BrowserRouter>
   );
 }
