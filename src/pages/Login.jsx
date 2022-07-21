d997239e9268ee69f624816acc6e04e94b6fd830
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-07-21 15:53:14 +0800

    Implement login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,57 @@
 import { useState } from "react";
 import { Container, Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/auth";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    if (loading) return;
+
+    setLoading(true);
+
+    const response = await fetch("http://localhost:4000/auth/login", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: new Headers({
+        "Content-Type": "application/json; charset=UTF-8",
+      }),
+    });
+
+    setLoading(false);
+
+    if (response.ok) {
+      const data = await response.json();
+      login(data.accessToken);
+      navigate("/");
+    } else {
+      const data = await response.json();
+      if (data && data.error) {
+        if (
+          data.error.code === "auth/user-not-found" ||
+          data.error.code === "auth/wrong-password"
+        ) {
+          alert("Username atau Password salah!");
+        }
+      }
+    }
+  }
 
   return (
     <Container className="mt-3">
       <h1>Login</h1>
 
-      <Form>
+      <Form onSubmit={onSubmit}>
         <Form.Group className="mb-3" controlId="username">
           <Form.Label>Username</Form.Label>
           <Form.Control
@@ -31,7 +73,7 @@ function Login() {
           />
         </Form.Group>
         <Button variant="primary" type="submit">
-          Submit
+          {loading ? "Loading..." : "Submit"}
         </Button>
       </Form>
     </Container>
